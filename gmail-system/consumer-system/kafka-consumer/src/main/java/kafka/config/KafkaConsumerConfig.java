2e8d13c9254be44ac9d8aef9506429269731488e
asudesozcu <sozcuasude@gmail.com>
2025-07-17 09:20:28 +0300

    LocalDateTime deserializer implemented for Consumer

--- gmail-system/consumer-system/kafka-consumer/src/main/java/kafka/config/KafkaConsumerConfig.java
+++ gmail-system/consumer-system/kafka-consumer/src/main/java/kafka/config/KafkaConsumerConfig.java
@@ -1,5 +1,8 @@
 package kafka.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import events.EmailFetchedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -25,26 +28,40 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
 
+    // ObjectMapper with JavaTimeModule for LocalDateTime support
     @Bean
-    public ConsumerFactory<String, EmailFetchedEvent> consumerFactory() {
-        JsonDeserializer<EmailFetchedEvent> deserializer = new JsonDeserializer<>(EmailFetchedEvent.class);
+    public ObjectMapper kafkaObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    @Bean
+    public ConsumerFactory<String, EmailFetchedEvent> consumerFactory(ObjectMapper kafkaObjectMapper) {
+        JsonDeserializer<EmailFetchedEvent> deserializer =
+                new JsonDeserializer<>(EmailFetchedEvent.class, kafkaObjectMapper, false);
         deserializer.addTrustedPackages("events");
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, EmailFetchedEvent> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, EmailFetchedEvent> kafkaListenerContainerFactory(
+            ConsumerFactory<String, EmailFetchedEvent> consumerFactory) {
+
         System.out.println("✅ kafkaListenerContainerFactory loaded");
 
-        ConcurrentKafkaListenerContainerFactory<String, EmailFetchedEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        ConcurrentKafkaListenerContainerFactory<String, EmailFetchedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
         return factory;
     }
 }
